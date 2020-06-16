3bc1aabfe602e83d9524f431159bb399856b16da
jgra007 <jr047x@gmail.com>
2020-06-16 02:56:48 +0000

    test5

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _islower - convert lowercase to 0 and 1
+ *
+ * @c: Value of input
+ *
+ * Return: 1 or 0
+ */
+int _islower(int c)
+{
+	if (c <= 'z' && c >= 'a')
+		return (1);
+	else
+		return (0);
+}
