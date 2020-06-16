eb0807e52817f3833e3b0d778827a4b8c0d2860a
jgra007 <jr047x@gmail.com>
2020-06-16 02:58:17 +0000

    test5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * print_sign - convert all letters to 1 else 0
+ *
+ * @n: Value of input number
+ *
+ * Return: 1 or 0 or -1
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
