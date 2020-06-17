5aa2870336e116656238418e159e0b15544f2d8b
jgra007 <jr047x@gmail.com>
2020-06-17 04:03:01 +0000

    test2

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -1,6 +1,8 @@
-#ifndef HOLBERTON
-#define HOLBERTON
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdio.h>
 
+void positive_or_negative(int i);
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
