7f5c0140b8f4d2643dc2cdef6cdeed84d2f98b17
jgra007 <jr047x@gmail.com>
2020-06-18 18:07:16 +0000

    test2

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - prints number followd by a new line
+ */
+void print_numbers(void)
+{
+	char num;
+
+	for (num = 48; num <=57; num++)
+	{
+		_putchar (num);
+	}
+	_putchar ('\n');
+}	
