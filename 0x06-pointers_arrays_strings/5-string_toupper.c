e5f3f9aa550922c37cd95b6e03e66315f2be7f4e
jgra007 <jr047x@gmail.com>
2020-06-25 23:05:21 +0000

    test1

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,31 @@
+#include "holberotn.h"
+
+/**
+ * _isalphalower - check for a lowercase or uppercase
+ * @c: the character to check
+ * Return: 1 if c is a lowercase or uppercase , 0 otherswise
+ */
+int _isalphalower(int c)
+
+{
+	return (c >= 'a' && c <= 'z');
+}
+
+/**
+ * string_toupper - change lowercase to uppercase
+ * @str: string to be changed to uppercase
+ * Return: str
+ */
+char *string_toupper(char *str)
+{
+	int pos;
+
+	for (pos = 0; str[pos] != '\0'; pos++)
+	{
+		if (_isalphalower(str[pos]))
+		{
+			str[pos] = str[pos] - 32;
+		}
+	}
+	return (str);
+}
