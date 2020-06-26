c35f962924ed1c1871f8f566395524baa46d20c2
jgra007 <jr047x@gmail.com>
2020-06-26 01:51:31 +0000

    test1

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * rot13 - encode a string in rot13
+ * @s: string to be encoded
+ * Return: the resulting string
+ */
+char *rot13(char *s)
+{
+	int x, y;
+
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		for (y = 0; a[y] != '\0' ; y++)
+		{
+			if (s[x] == a[y])
+			{
+				s[x] = b[y];
+			}
+		}
+	}
+	return (s);
+}
