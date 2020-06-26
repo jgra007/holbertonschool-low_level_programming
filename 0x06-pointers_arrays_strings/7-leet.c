8c446ce09b1fc7535c91a59b53ae4975f4af5065
jgra007 <jr047x@gmail.com>
2020-06-26 01:28:28 +0000

    test1

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * leet - encodes a strin into 1337
+ * @s: variable to be used
+ * Return: s
+ */
+char *leet(char *s)
+{
+	int x, y;
+
+	char *a = "aAeEoOtTlL";
+	char *b = "4433007711";
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			if (s[x] == a[y])
+			{
+				s[x] == b[y];
+			}
+		}
+	}
+	return (s);
+}
