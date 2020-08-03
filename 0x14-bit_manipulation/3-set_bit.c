f84ea19ffe5e5e1e66ab9667fc0c95e1bda8b949
jgra007 <jr047x@gmail.com>
2020-08-03 02:30:15 +0000

    test6.3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,16 @@
+nclude "holberton.h"
+
+/**
+ * set_bit - function to set a bit
+ * @n: int pointer
+ * @index: index at bit to change
+ * Return: 1 if sucessful -1 if failed
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 64)
+		return (-1);
+
+	*n |= (1 << index);
+	return (1);
+}
