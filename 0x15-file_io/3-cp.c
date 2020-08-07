ade211ab9d9f35c4babe664a7dcb10919c3c0c4b
jgra007 <jr047x@gmail.com>
2020-08-07 02:53:44 +0000

    test7.5

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,7 +16,7 @@ int main(int argc, char **argv)
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE %s\n",
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
 		 argv[1]);
 		exit(98);
 	}
